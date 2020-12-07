854b414e178d12450520f8c042b5aba980d289b1
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2020-12-06 22:03:38 -0800

    feat: display system information (#4)

--- src/renderer/screens/Home/Home.tsx
+++ src/renderer/screens/Home/Home.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import * as osUtils from 'node-os-utils'
 
 import { CPUStat } from './components/CPUStat'
 import { Title } from './components/Title'
@@ -10,6 +11,14 @@ type HomeProps = {
   selectedTab: 'cpu' | 'system' | 'settings'
 }
 
+function formatUptime(uptimeInSeconds: number) {
+  const d = Math.floor(uptimeInSeconds / (3600 * 24))
+  const h = Math.floor((uptimeInSeconds % (3600 * 24)) / 3600)
+  const m = Math.floor((uptimeInSeconds % 3600) / 60)
+  const s = Math.floor(uptimeInSeconds % 60)
+  return `${d}d, ${h}h, ${m}m, ${s}s`
+}
+
 export const Home: React.FC<HomeProps> = ({ selectedTab }) => {
   const [tabsVisibility, setTabsVisibility] = React.useState({
     cpu: true,
@@ -17,31 +26,52 @@ export const Home: React.FC<HomeProps> = ({ selectedTab }) => {
     settings: false,
   })
 
+  const [cpuUsage, setCpuUsage] = React.useState(0)
+  const [cpuFree, setCpuFree] = React.useState(0)
+  const [cpuUptime, setCpuUptime] = React.useState(0)
+  const [totalMemory, setTotalMemory] = React.useState(0)
+
   React.useEffect(() => {
     setTabsVisibility({ cpu: false, system: false, settings: false, [selectedTab]: true })
   }, [selectedTab])
+
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      osUtils.cpu.usage().then(setCpuUsage).catch(console.error)
+      osUtils.cpu.free().then(setCpuFree).catch(console.error)
+      setCpuUptime(osUtils.os.uptime())
+    }, 1500)
+
+    osUtils.mem.info().then((info) => {
+      setTotalMemory(info.totalMemMb)
+    })
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
 
   return (
     <main>
       <div className={`content ${tabsVisibility.cpu ? 'show' : ''}`}>
         <Title title="CPU" icon="fa-microchip" />
         <div className="progress-bar">
-          <div className="progress" id="cpu-progress"></div>
+          <div className="progress" id="cpu-progress" style={{ width: cpuUsage + '%' }}></div>
         </div>
 
         <ul>
-          <CPUStat title="CPU Usage" />
-          <CPUStat title="CPU Free" />
+          <CPUStat title="CPU Usage" stat={cpuUsage + '%'} />
+          <CPUStat title="CPU Free" stat={cpuFree + '%'} />
         </ul>
-        <div id="cpu-model"></div>
+        <div id="cpu-model">{osUtils.cpu.model()}</div>
       </div>
       <div className={`content ${tabsVisibility.system ? 'show' : ''}`}>
         <Title title="System Info" icon="fa-info" />
         <ul>
-          <CPUStat title="Computer Name" />
-          <CPUStat title="OS" />
-          <CPUStat title="System Uptime" />
-          <CPUStat title="System Memory" />
+          <CPUStat title="Computer Name" stat={osUtils.os.hostname()} />
+          <CPUStat title="OS" stat={`${osUtils.os.type()} ${osUtils.os.arch()}`} />
+          <CPUStat title="System Uptime" stat={formatUptime(cpuUptime)} />
+          <CPUStat title="System Memory" stat={totalMemory + ' MB'} />
         </ul>
       </div>
       <div className={`content ${tabsVisibility.settings ? 'show' : ''}`}>
